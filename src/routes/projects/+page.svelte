2121c0a629de45a7e06362f2634342b6c514091b
Iqbal <iqbal.assignment@gmail.com>
2026-02-04 15:18:55 +0700

    feat(projects): add projects page with hero section and projects list

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Projects from '$lib/components/sections/home/Projects.svelte';
+	import heroBackground from '$lib/assets/images/terracing.jpeg';
+	import Hero from '$lib/components/ui/Hero.svelte';
+</script>
+
+<div>
+	<Hero backgroundImage={heroBackground} class="aspect-[2.77/1] bg-center">
+		<div class="space-y-4 font-faculty-glyphic">
+			<h1 class="text-4xl leading-tight md:text-6xl lg:text-[76px]">Projects</h1>
+		</div>
+	</Hero>
+	<Projects />
+</div>
